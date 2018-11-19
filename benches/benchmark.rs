c8de938eaee4956297e8c34d35015aa67f97e5d5
Florian Marending <floriama@student.ethz.ch>
2018-11-19 13:13:01 +0100

    Benchmark colorings

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,19 +1,66 @@
 #[macro_use]
 extern crate criterion;
+extern crate graml;
 
 use criterion::Criterion;
+use criterion::Fun;
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
+use graml::graph::EdgeList;
+use graml::graph::AdjMatrix;
+
+use graml::coloring::*;
+
+fn edgelist_creation(n: usize) {
+    let g = EdgeList::random(n, 0.5);
+
+    let num_edges = g.edges().count();
+
+    assert!(num_edges > 1);
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(20)));
+fn adj_creation(n: usize) {
+    let g = AdjMatrix::random(n, 0.5);
+
+    let num_edges = g.edges().count();
+
+    assert!(num_edges > 1);
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn edgelist_rs(n: usize) {
+    let g = EdgeList::random(n, 0.5);
+    let _ = rs_coloring(&g);
+}
+
+fn edgelist_cs(n: usize) {
+    let g = EdgeList::random(n, 0.5);
+    let _ = cs_coloring(&g);
+}
+
+fn edgelist_lf(n: usize) {
+    let g = EdgeList::random(n, 0.5);
+    let _ = lf_coloring(&g);
+}
+
+fn edgelist_sl(n: usize) {
+    let g = EdgeList::random(n, 0.5);
+    let _ = sl_coloring(&g);
+}
+
+fn graphs(c: &mut Criterion) {
+    let edgelist = Fun::new("EdgeList", |b, i| b.iter(|| edgelist_creation(*i)));
+    let adjmatrix = Fun::new("AdjMatrix", |b, i| b.iter(|| adj_creation(*i)));
+
+    let functions = vec!(edgelist, adjmatrix);
+    c.bench_functions("Graph Creation", functions, 100);
+
+    let rs = Fun::new("RS", |b, i| b.iter(|| edgelist_rs(*i)));
+    let cs = Fun::new("CS", |b, i| b.iter(|| edgelist_cs(*i)));
+    let lf = Fun::new("LF", |b, i| b.iter(|| edgelist_lf(*i)));
+    let sl = Fun::new("SL", |b, i| b.iter(|| edgelist_sl(*i)));
+
+    let functions = vec!(rs, cs, lf, sl);
+    c.bench_functions("Graph Coloring", functions, 100);
+}
+
+criterion_group!(benches, graphs);
 criterion_main!(benches);
