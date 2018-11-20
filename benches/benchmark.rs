595db06b0db80f821db5d1e885cb0f5b4e7275fd
Florian Marending <floriama@student.ethz.ch>
2018-11-20 13:19:01 +0100

    Clean up benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -7,96 +7,97 @@ use criterion::Fun;
 
 use graml::graph::EdgeList;
 use graml::graph::AdjMatrix;
+use graml::graph::GrowableAdjMatrix;
 
 use graml::coloring::*;
 use graml::graph::Graph;
 
-fn edgelist_creation(n: usize) {
-    let g = EdgeList::random(n, 0.5);
+fn edgelist_creation(n: usize, p: f32) {
+    let g = EdgeList::random(n, p);
 
     let num_edges = g.edges().count();
 
     assert!(num_edges > 1);
 }
 
-fn adj_creation(n: usize) {
-    let g = AdjMatrix::random(n, 0.5);
+fn adj_creation(n: usize, p: f32) {
+    let g = AdjMatrix::random(n, p);
 
     let num_edges = g.edges().count();
 
     assert!(num_edges > 1);
 }
 
-// EdgeList
-fn edgelist_rs(n: usize) {
-    let g = EdgeList::random(n, 0.5);
-    let _ = rs_coloring(&g);
+fn gadj_creation(n: usize, p: f32) {
+    let g = GrowableAdjMatrix::random(n, p);
+
+    let num_edges = g.edges().count();
+
+    assert!(num_edges > 1);
 }
 
-fn edgelist_cs(n: usize) {
-    let g = EdgeList::random(n, 0.5);
-    let _ = cs_coloring(&g);
+#[derive(Copy, Clone)]
+enum C {
+    RS,
+    CS,
+    LF,
+    SL,
 }
 
-fn edgelist_lf(n: usize) {
-    let g = EdgeList::random(n, 0.5);
-    let _ = lf_coloring(&g);
-}
-
-fn edgelist_sl(n: usize) {
-    let g = EdgeList::random(n, 0.5);
-    let _ = sl_coloring(&g);
-}
-
-// AdjMatrix
-fn adj_rs(n: usize) {
-    let g = AdjMatrix::random(n, 0.5);
-    let _ = rs_coloring(&g);
-}
-
-fn adj_cs(n: usize) {
-    let g = AdjMatrix::random(n, 0.5);
-    let _ = cs_coloring(&g);
-}
-
-fn adj_lf(n: usize) {
-    let g = AdjMatrix::random(n, 0.5);
-    let _ = lf_coloring(&g);
-}
-
-fn adj_sl(n: usize) {
-    let g = AdjMatrix::random(n, 0.5);
-    let _ = sl_coloring(&g);
+fn colorer<G: Graph>(c: C, n: usize, p: f32) {
+    let g = G::random(n, p);
+    match c {
+        C::RS => rs_coloring(&g),
+        C::CS => cs_coloring(&g),
+        C::LF => lf_coloring(&g),
+        C::SL => sl_coloring(&g),
+    };
 }
 
 fn graphs(c: &mut Criterion) {
-    let edgelist = Fun::new("EdgeList", |b, i| b.iter(|| edgelist_creation(*i)));
-    let adjmatrix = Fun::new("AdjMatrix", |b, i| b.iter(|| adj_creation(*i)));
+    let n = 200;
+    let p = 0.2;
 
-    let functions = vec!(edgelist, adjmatrix);
-    c.bench_functions("Graph Creation", functions, 100);
+    let edgelist = Fun::new("EdgeList", move |b, i| b.iter(|| edgelist_creation(*i, p)));
+    let adjmatrix = Fun::new("AdjMatrix", move |b, i| b.iter(|| adj_creation(*i, p)));
+    let gadjmatrix = Fun::new("GrowableAdjMatrix", move |b, i| b.iter(|| gadj_creation(*i, p)));
 
-    let rs = Fun::new("RS", |b, i| b.iter(|| edgelist_rs(*i)));
-    let cs = Fun::new("CS", |b, i| b.iter(|| edgelist_cs(*i)));
-    let lf = Fun::new("LF", |b, i| b.iter(|| edgelist_lf(*i)));
-    let sl = Fun::new("SL", |b, i| b.iter(|| edgelist_sl(*i)));
+    let functions = vec!(edgelist, adjmatrix, gadjmatrix);
+    c.bench_functions("Graph Creation", functions, n);
+
+    let n = 100;
+    let p = 0.2;
+
+    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<EdgeList>(C::RS, n, p)));
+    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<EdgeList>(C::CS, n, p)));
+    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<EdgeList>(C::LF, n, p)));
+    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<EdgeList>(C::SL, n, p)));
 
     let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring EdgeList", functions, 100);
+    c.bench_functions("Graph Coloring EdgeList", functions, 0);
 
-    let rs = Fun::new("RS", |b, i| b.iter(|| adj_rs(*i)));
-    let cs = Fun::new("CS", |b, i| b.iter(|| adj_cs(*i)));
-    let lf = Fun::new("LF", |b, i| b.iter(|| adj_lf(*i)));
-    let sl = Fun::new("SL", |b, i| b.iter(|| adj_sl(*i)));
+    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::RS, n, p)));
+    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::CS, n, p)));
+    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::LF, n, p)));
+    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::SL, n, p)));
 
     let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring AdjMatrix", functions, 100);
+    c.bench_functions("Graph Coloring AdjMatrix", functions, 0);
 
-    let sl_el = Fun::new("EdgeList", |b, i| b.iter(|| edgelist_sl(*i)));
-    let sl_am = Fun::new("AdjMatrix", |b, i| b.iter(|| adj_sl(*i)));
+    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::RS, n, p)));
+    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::CS, n, p)));
+    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::LF, n, p)));
+    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::SL, n, p)));
 
-    let functions = vec![sl_el, sl_am];
-    c.bench_functions("Graph Coloring SL", functions, 100);
+    let functions = vec!(rs, cs, lf, sl);
+    c.bench_functions("Graph Coloring GrowableAdjMatrix", functions, 0);
+
+    let sl_el = Fun::new("EdgeList", move |b, _| b.iter(|| colorer::<EdgeList>(C::SL, n, p)));
+    let sl_am = Fun::new("AdjMatrix", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::SL, n, p)));
+    let sl_gam = Fun::new("GrowableAdjMatrix", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::SL, n, p)));
+
+    let functions = vec![sl_el, sl_am, sl_gam];
+    c.bench_functions("Graph Coloring SL", functions, 0);
 }
 
 criterion_group!(benches, graphs);
