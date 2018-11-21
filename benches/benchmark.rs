4574d0dd1ab24f4361e448bd4f0f091ab9a33cac
Florian Marending <floriama@student.ethz.ch>
2018-11-21 13:56:40 +0100

    Make sure we only time coloring, not construction

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -5,14 +5,8 @@ extern crate graml;
 use criterion::Criterion;
 use criterion::Fun;
 
-use graml::graph::EdgeList;
-use graml::graph::AdjMatrix;
-use graml::graph::GrowableAdjMatrix;
-use graml::graph::AdjList;
-use graml::graph::Hybrid;
-
 use graml::coloring::*;
-use graml::graph::StaticGraph;
+use graml::graph::*;
 
 fn edgelist_creation(n: usize, p: f32) {
     let g = EdgeList::random(n, p);
@@ -62,13 +56,12 @@ enum C {
     SL,
 }
 
-fn colorer<G: StaticGraph>(c: C, n: usize, p: f32) {
-    let g = G::random(n, p);
+fn colorer<G: StaticGraph>(c: C, g: &G) {
     match c {
-        C::RS => rs_coloring(&g),
-        C::CS => cs_coloring(&g),
-        C::LF => lf_coloring(&g),
-        C::SL => sl_coloring(&g),
+        C::RS => rs_coloring(g),
+        C::CS => cs_coloring(g),
+        C::LF => lf_coloring(g),
+        C::SL => sl_coloring(g),
     };
 }
 
@@ -82,89 +75,51 @@ fn graphs(c: &mut Criterion) {
     let adjlmatrix = Fun::new("AdjList", move |b, i| b.iter(|| adjl_creation(*i, p)));
     let hybrid = Fun::new("Hybrid", move |b, i| b.iter(|| hybrid_creation(*i, p)));
 
-    let functions = vec!(edgelist, adjmatrix, gadjmatrix, adjlmatrix);
+    let functions = vec!(edgelist, adjmatrix, gadjmatrix, adjlmatrix, hybrid);
     c.bench_functions("Graph Creation", functions, n);
 
     let n = 50;
     let p = 0.2;
 
-    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<EdgeList>(C::RS, n, p)));
-    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<EdgeList>(C::CS, n, p)));
-    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<EdgeList>(C::LF, n, p)));
-    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<EdgeList>(C::SL, n, p)));
+    let rs = Fun::new("RS", move |b, g| b.iter(|| colorer::<EdgeList>(C::RS, g)));
+    let cs = Fun::new("CS", move |b, g| b.iter(|| colorer::<EdgeList>(C::CS, g)));
+    let lf = Fun::new("LF", move |b, g| b.iter(|| colorer::<EdgeList>(C::LF, g)));
+    let sl = Fun::new("SL", move |b, g| b.iter(|| colorer::<EdgeList>(C::SL, g)));
 
     let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring EdgeList", functions, 0);
+    c.bench_functions("Graph Coloring EdgeList", functions, EdgeList::random(n,p));
 
-    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::RS, n, p)));
-    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::CS, n, p)));
-    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::LF, n, p)));
-    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::SL, n, p)));
-
-    let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring AdjMatrix", functions, 0);
-
-    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::RS, n, p)));
-    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::CS, n, p)));
-    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::LF, n, p)));
-    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::SL, n, p)));
+    let rs = Fun::new("RS", move |b, g| b.iter(|| colorer::<AdjMatrix>(C::RS, g)));
+    let cs = Fun::new("CS", move |b, g| b.iter(|| colorer::<AdjMatrix>(C::CS, g)));
+    let lf = Fun::new("LF", move |b, g| b.iter(|| colorer::<AdjMatrix>(C::LF, g)));
+    let sl = Fun::new("SL", move |b, g| b.iter(|| colorer::<AdjMatrix>(C::SL, g)));
 
     let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring GrowableAdjMatrix", functions, 0);
+    c.bench_functions("Graph Coloring AdjMatrix", functions, AdjMatrix::random(n,p));
 
-    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<AdjList>(C::RS, n, p)));
-    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<AdjList>(C::CS, n, p)));
-    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<AdjList>(C::LF, n, p)));
-    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<AdjList>(C::SL, n, p)));
-
-    let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring AdjList", functions, 0);
-
-    let rs = Fun::new("RS", move |b, _| b.iter(|| colorer::<Hybrid>(C::RS, n, p)));
-    let cs = Fun::new("CS", move |b, _| b.iter(|| colorer::<Hybrid>(C::CS, n, p)));
-    let lf = Fun::new("LF", move |b, _| b.iter(|| colorer::<Hybrid>(C::LF, n, p)));
-    let sl = Fun::new("SL", move |b, _| b.iter(|| colorer::<Hybrid>(C::SL, n, p)));
+    let rs = Fun::new("RS", move |b, g| b.iter(|| colorer::<GrowableAdjMatrix>(C::RS, g)));
+    let cs = Fun::new("CS", move |b, g| b.iter(|| colorer::<GrowableAdjMatrix>(C::CS, g)));
+    let lf = Fun::new("LF", move |b, g| b.iter(|| colorer::<GrowableAdjMatrix>(C::LF, g)));
+    let sl = Fun::new("SL", move |b, g| b.iter(|| colorer::<GrowableAdjMatrix>(C::SL, g)));
 
     let functions = vec!(rs, cs, lf, sl);
-    c.bench_functions("Graph Coloring Hybrid", functions, 0);
+    c.bench_functions("Graph Coloring GrowableAdjMatrix", functions, GrowableAdjMatrix::random(n,p));
 
-    // ---------------------- //
+    let rs = Fun::new("RS", move |b, g| b.iter(|| colorer::<AdjList>(C::RS, g)));
+    let cs = Fun::new("CS", move |b, g| b.iter(|| colorer::<AdjList>(C::CS, g)));
+    let lf = Fun::new("LF", move |b, g| b.iter(|| colorer::<AdjList>(C::LF, g)));
+    let sl = Fun::new("SL", move |b, g| b.iter(|| colorer::<AdjList>(C::SL, g)));
 
-    let el = Fun::new("EdgeList", move |b, _| b.iter(|| colorer::<EdgeList>(C::RS, n, p)));
-    let am = Fun::new("AdjMatrix", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::RS, n, p)));
-    let gam = Fun::new("GrowableAdjMatrix", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::RS, n, p)));
-    let adl = Fun::new("AdjList", move |b, _| b.iter(|| colorer::<AdjList>(C::RS, n, p)));
-    let hyb = Fun::new("Hybrid", move |b, _| b.iter(|| colorer::<Hybrid>(C::RS, n, p)));
+    let functions = vec!(rs, cs, lf, sl);
+    c.bench_functions("Graph Coloring AdjList", functions, AdjList::random(n,p));
 
-    let functions = vec![el, am, gam, adl, hyb];
-    c.bench_functions("Graph Coloring RS", functions, 0);
+    let rs = Fun::new("RS", move |b, g| b.iter(|| colorer::<Hybrid>(C::RS, g)));
+    let cs = Fun::new("CS", move |b, g| b.iter(|| colorer::<Hybrid>(C::CS, g)));
+    let lf = Fun::new("LF", move |b, g| b.iter(|| colorer::<Hybrid>(C::LF, g)));
+    let sl = Fun::new("SL", move |b, g| b.iter(|| colorer::<Hybrid>(C::SL, g)));
 
-    let el = Fun::new("EdgeList", move |b, _| b.iter(|| colorer::<EdgeList>(C::CS, n, p)));
-    let am = Fun::new("AdjMatrix", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::CS, n, p)));
-    let gam = Fun::new("GrowableAdjMatrix", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::CS, n, p)));
-    let adl = Fun::new("AdjList", move |b, _| b.iter(|| colorer::<AdjList>(C::CS, n, p)));
-    let hyb = Fun::new("Hybrid", move |b, _| b.iter(|| colorer::<Hybrid>(C::CS, n, p)));
-
-    let functions = vec![el, am, gam, adl, hyb];
-    c.bench_functions("Graph Coloring CS", functions, 0);
-
-    let el = Fun::new("EdgeList", move |b, _| b.iter(|| colorer::<EdgeList>(C::LF, n, p)));
-    let am = Fun::new("AdjMatrix", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::LF, n, p)));
-    let gam = Fun::new("GrowableAdjMatrix", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::LF, n, p)));
-    let adl = Fun::new("AdjList", move |b, _| b.iter(|| colorer::<AdjList>(C::LF, n, p)));
-    let hyb = Fun::new("Hybrid", move |b, _| b.iter(|| colorer::<Hybrid>(C::LF, n, p)));
-
-    let functions = vec![el, am, gam, adl, hyb];
-    c.bench_functions("Graph Coloring LF", functions, 0);
-
-    let el = Fun::new("EdgeList", move |b, _| b.iter(|| colorer::<EdgeList>(C::SL, n, p)));
-    let am = Fun::new("AdjMatrix", move |b, _| b.iter(|| colorer::<AdjMatrix>(C::SL, n, p)));
-    let gam = Fun::new("GrowableAdjMatrix", move |b, _| b.iter(|| colorer::<GrowableAdjMatrix>(C::SL, n, p)));
-    let adl = Fun::new("AdjList", move |b, _| b.iter(|| colorer::<AdjList>(C::SL, n, p)));
-    let hyb = Fun::new("Hybrid", move |b, _| b.iter(|| colorer::<Hybrid>(C::SL, n, p)));
-
-    let functions = vec![el, am, gam, adl, hyb];
-    c.bench_functions("Graph Coloring SL", functions, 0);
+    let functions = vec!(rs, cs, lf, sl);
+    c.bench_functions("Graph Coloring Hybrid", functions, Hybrid::random(n,p));
 }
 
 criterion_group!(benches, graphs);
