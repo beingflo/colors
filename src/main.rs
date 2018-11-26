842232eebd06b4c5ae44e3612f2ce61534b8aace
Florian Marending <floriama@student.ethz.ch>
2018-11-26 13:28:11 +0100

    Restructure main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 extern crate graml;
 extern crate num_cpus;
+extern crate crossbeam;
 
-use std::thread;
 use std::env;
 use std::fs;
+use std::thread;
 use std::path::Path;
-use std::sync::mpsc;
 
 use graml::graph::*;
 use graml::coloring::*;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
+
+    let mut graphs = Vec::new();
+    let mut names = Vec::new();
+
     if args.len() == 1 {
         // Run comparison on this many graphs
         let samples = 50;
@@ -22,7 +26,8 @@ fn main() {
         // Edge probability in each sample graph
         let p = 0.9;
 
-        comparison(samples, n, p);
+        graphs = (0..samples).map(move |_| Graph::random(n, p)).collect::<Vec<Graph>>();
+        names = (0..samples).map(move |_| format!("Random({},{})", n, p).into()).collect::<Vec<String>>();
     } else {
         let path = &args[1];
         let meta = fs::metadata(path).unwrap();
@@ -32,73 +37,70 @@ fn main() {
             for file in fs::read_dir(path).unwrap() {
                 let file = file.unwrap().path();
                 let g = load_graph(file.clone()).unwrap();
+                let name = file.file_name().unwrap().to_str().unwrap();
 
-                println!("Graph {} with {} vertices\n", file.file_name().unwrap().to_str().unwrap(), g.num_vertices());
-
-                let (n1, n2, n3, n4, n5) = all_colorings(&g);
-
-                println!("rs\tcs\tlf\tsl\tsdo");
-                println!("{}\t{}\t{}\t{}\t{}\n", n1, n2, n3, n4, n5);
+                graphs.push(g);
+                names.push(name.into());
             }
         } else {
             let file = Path::new(path);
             let g = load_graph(file.clone()).unwrap();
+            let name = file.file_name().unwrap().to_str().unwrap();
 
-            println!("Graph {} with {} vertices\n", file.file_name().unwrap().to_str().unwrap(), g.num_vertices());
-
-            let (n1, n2, n3, n4, n5) = all_colorings(&g);
-
-            println!("rs\tcs\tlf\tsl\tsdo\n");
-            println!("{}\t{}\t{}\t{}\t{}", n1, n2, n3, n4, n5);
-
+            graphs.push(g);
+            names.push(name.into());
         }
     }
+
+    parallel_coloring(graphs, names);
 }
 
-fn comparison(samples: usize, n: usize, p: f32) {
+fn parallel_coloring<G: StaticGraph + Send + 'static>(graphs: Vec<G>, names: Vec<String>) {
+    assert_eq!(graphs.len(), names.len());
+    let samples = graphs.len();
+
     // Number of processors
     let workers = num_cpus::get();
 
     // Communication channel between worker threads and main thread
-    let (tx, rx) = mpsc::channel();
+    let (tx_res, rx_res) = crossbeam::unbounded();
+    let (tx_job, rx_job) = crossbeam::unbounded();
 
-    for i in 0..workers {
-        // Each worker performs comparison on this many graphs
-        let mut slice = samples / workers;
+    let mut max_width = 0;
+    for (graph, name) in graphs.into_iter().zip(names.into_iter()) {
+        max_width = max_width.max(name.len());
+        tx_job.send((graph, name)).unwrap();
+    }
 
-        // Last worker picks up remaining samples
-        if i == workers - 1 {
-            slice += samples - (samples / workers) * workers;
-        }
-
-        let tx_ = tx.clone();
+    for _ in 0..workers {
+        let tx_ = tx_res.clone();
+        let rx_ = rx_job.clone();
 
         // Spawn workers
-        let _ = thread::spawn(move || {
-            for _ in 0..slice {
-                // Create new graph
-                let g = Graph::random(n, p);
+        thread::spawn(move || {
+            for (graph, name) in rx_.iter() {
+                // Color graph
+                let c = all_colorings(&graph);
 
-                let c = all_colorings(&g);
-
-                // Send to main thread
-                tx_.send(c).unwrap();
+                // Send result back to main thread
+                tx_.send((c, name)).unwrap();
             }
         });
     }
 
-    // Drop original tx such that rx.iter() will yield `None` when last tx_ is dropped
-    drop(tx);
+    // Drop original tx_job such that rx_.iter() will yield `None` when last tx_ is dropped
+    drop(tx_job);
+    drop(tx_res);
 
     // Print results
-    println!("Random graph with {} vertices and {} edge probability\n", n, p);
-    println!("rs\tcs\tlf\tsl\tsdo\n");
+    let spacing = 8;
+    println!("{0:<1$}{2:>7$}{3:>7$}{4:>7$}{5:>7$}{6:>7$}", "", max_width, "rs", "cs", "lf", "sl", "sdo", spacing);
 
     let mut sum = [0; 5];
 
     // Iterate over all values received by worker threads
-    for (n1, n2, n3, n4, n5) in rx.iter() {
-        println!("{}\t{}\t{}\t{}\t{}", n1, n2, n3, n4, n5);
+    for ((n1, n2, n3, n4, n5), name) in rx_res.iter() {
+        println!("{0:<1$}{2:>7$}{3:>7$}{4:>7$}{5:>7$}{6:>7$}", name, max_width, n1, n2, n3, n4, n5, spacing);
 
         sum[0] += n1;
         sum[1] += n2;
@@ -108,9 +110,9 @@ fn comparison(samples: usize, n: usize, p: f32) {
     }
 
 
-    println!("\n{}\t{}\t{}\t{}\t{}", sum[0] as f32/samples as f32,
+    println!("\n{0:<1$}{2:>7$}{3:>7$}{4:>7$}{5:>7$}{6:>7$}", "", max_width, sum[0] as f32/samples as f32,
              sum[1] as f32/samples as f32, sum[2] as f32/samples as f32,
-             sum[3] as f32/samples as f32, sum[4] as f32/samples as f32);
+             sum[3] as f32/samples as f32, sum[4] as f32/samples as f32, spacing);
 }
 
 fn all_colorings<G: StaticGraph>(g: &G) -> (usize, usize, usize, usize, usize) {
